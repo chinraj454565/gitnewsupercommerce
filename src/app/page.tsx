07fa5cf64e7de4df8eb51fea52811eb50067121c
Chinraj P <jarchin6@gmail.com>
2026-02-09 10:40:13 +0530

    git-update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Hero } from "@/components/sections/Hero";
+import { Hero } from "@/components/sections/hero";
 import { Testimonial } from "@/components/sections/Testimonial-Section";
 import { HighlightSection } from "@/components/sections/Highlight-Section";
 import { TestimonialTwo } from "@/components/sections/Testimonial-Two";
